2e713f455c5c5b7db3bf83f329c7399c6a6c3b36
K. Malice <113646116+Kyle-Malice@users.noreply.github.com>
2023-07-18 22:37:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+Student Name: Kyle Malice
+File Name: script.js
+Date Edited: 04/09/2023
+*/
+
+//Hamburger Menu Function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    var logo = document.getElementById("ffc-logo");
+    if (menu.style.display === "block" && logo.style.display=== "none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+        menu.style.display = "block";
+        logo.style.display = "none";
+    }
+}
